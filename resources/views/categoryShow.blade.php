1c544a30fb91d4c40b0c9d5ec5c77bfa0ee317f2
Atajan <aatajan550@gmail.com>
2024-05-05 20:30:22 +0500

    1.0.4

--- resources/views/categoryShow.blade.php
+++ resources/views/categoryShow.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @include("category.category")
     {{--@include("brand.brand")--}}
-    <div class="row row-cols-4 g-2">
+    <div class="row row-cols-2 row-cols-md-3 row-cols-xl-4 m-auto g-2 justify-content-center">
         @foreach($products as $product)
             <div class="col">
                 @include('app.product')
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -2,15 +2,17 @@
 @section('title') {{$productShow->name}} @endsection
 @section('content')
     @include("category.category")
-    <div class="d-flex justify-content-around bg-primary bg-opacity-10 h-100">
-        <img class="m-3 rounded border shadow h-25 w-25 col-8" src="{{$productShow->getImage()}}">
-        <div class="d-flex flex-column my-5 col-6">
-            <div class="mt-5 p-2 h3 h-25 text-center ">
+    <div class="row row-cols-lg-2 bg-primary bg-opacity-10 mt-2">
+        <div class=" d-flex">
+        <img class="m-3 rounded border shadow mx-sm-auto" src="{{$productShow->getImage()}}">
+        </div>
+        <div class="my-auto ">
+            <div class="p-2 h3 text-center ">
                 {{$productShow->name}}
             </div>
             <div class="mt-2 fw-bold {{$productShow->discount ? 'text-danger' : 'text-primary' }} text-center h2">
                 {{$productShow->discount ? $productShow->price - ($productShow->price * ($productShow->discount/100)) : $productShow->price}}</div>
-            <div class=" p-2 h3 h-25 text-center">
+            <div class=" h3 text-center">
                 <div class="mt-2 fw-bold text-decoration-line-through text-secondary text-center">
                     {!! $productShow->discount ? $productShow->price : '<br>'!!}
                 </div>
@@ -23,28 +25,9 @@
     </div>
 
 
-    <div class="row row-cols-4 g-2">
+    <div class="row row-cols-2 row-cols-md-3 row-cols-xl-4 mt-3 g-2">
         @foreach($recomends as $product)
             <div class="col">
-                {{--<div class="rounded bg-secondary bg-opacity-10 p-3"><a class="nav-link link-light fw-bold px-3">--}}
-                        {{--<img src="{{$recomend->getImage()}}" class="img-fluid rounded">--}}
-                        {{--<div class="mt-2 text-primary small text-center">--}}
-                            {{--{{$product->name}}--}}
-                        {{--</div>--}}
-
-                        {{--<div class="mt-2 fw-bold {{$recomend->discount ? 'text-danger' : 'text-primary' }} text-center h2">--}}
-                            {{--{{$recomend->discount ? $recomend->price - ($recomend->price * ($recomend->discount/100)) : $recomend->price}}</div>--}}
-                        {{--<div class=" p-2 h3 h-25 text-center">--}}
-                            {{--<div class="mt-2 fw-bold text-decoration-line-through text-secondary text-center">--}}
-                                {{--{!! $recomend->discount ? $recomend->price : '<br>'!!}--}}
-                            {{--</div>--}}
-                            {{--<p class="card-text">--}}
-                                {{--<small class="text-muted small text-end">Updated--}}
-                                    {{--At {{$recomend->updated_at->format('d.m.Y')}}</small>--}}
-                            {{--</p>--}}
-                        {{--</div>--}}
-                    {{--</a>--}}
-                {{--</div>--}}
                 @include('app.product')
             </div>
 
